ea234e2ae775b22fe63f8e04be9a402243cf7884
SolusCodex <138786760+SolusCodex@users.noreply.github.com>
2023-07-24 10:11:10 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -81,7 +81,7 @@
     // Portfolio isotope and filter
     var portfolioIsotope = $('.portfolio-container').isotope({
         itemSelector: '.portfolio-item',
-        layoutMode: 'fitRows'
+        layoutMode: 'masonry'
     });
     $('#portfolio-flters li').on('click', function () {
         $("#portfolio-flters li").removeClass('active');
